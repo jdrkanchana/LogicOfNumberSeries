4fd5cce33996f82fcfeeeb8ccc2d0376ae94f3b8
jdrkanchana <66084534+jdrkanchana@users.noreply.github.com>
2020-09-12 14:07:39 +0530

    Create second_highest_num_Array.py

---
+++ second_highest_num_Array.py
@@ -0,0 +1,6 @@
+array=[2,5,16,20,25]
+length=len(array)
+for a in range(0,(length)):
+    if(a==(length-2)):
+        second_highest=array[a]
+print("the second highest number in the array is", second_highest)
