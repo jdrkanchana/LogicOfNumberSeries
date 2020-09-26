43236eace117cddd7dea0059992dc9466c15b5ed
jdrkanchana <66084534+jdrkanchana@users.noreply.github.com>
2020-09-26 14:48:54 +0530

    Create number_to_text.py

---
+++ number_to_text.py
@@ -0,0 +1,21 @@
+phone="1234"
+for number in phone:
+	if number=="1":
+		print("one ", end='')
+	elif number=="2":
+		print("two ", end='')
+	elif number=="3":
+		print("three ", end='')
+	elif number=="4":
+		print("four ", end='')
+	elif number=="5":
+		print("five ", end='')
+	elif number=="6":
+		print("six ", end='')
+	elif number=="7":
+		print("seven ", end='')
+	elif number=="8":
+		print("eight ", end='')
+	elif number=="9":
+		print("nine ", end='')
+	
