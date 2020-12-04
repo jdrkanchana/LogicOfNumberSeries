49f1f88aeb3bea29f51fc4187b9eba428cbe3481
jdrkanchana <66084534+jdrkanchana@users.noreply.github.com>
2020-12-04 14:54:25 +0530

    Create index.py

---
+++ file/index.py
@@ -0,0 +1,31 @@
+
+game="Snakes and Ladders"
+print(game[0:2])
+# outputs in game 0th till 2nd index elements
+
+print(game[2:7])
+# outputs in game 2th till 7nd index elements
+
+print(game[-1])
+#outputs the last element in game word
+
+print(game[-2])
+#outputs the one before last element in game word
+
+print(game.upper())
+#outputs the entire game word in block capital letters
+
+print(game.lower())
+#outputs the entire game word in simple letters
+
+print(game)
+#yet the game word hasnt changed using upper() or lower() functions on the string word
+
+print(game.find('L'))
+#fimd the index number of the game word
+
+print(game.replace('Snakes','Monkeys'))
+# replace the word Snake with word Monkey
+
+print(course.replace('snakes','Smiley'))
+#no output change 
