c79fec46ced47625780b45f7ee759832f122686f
jdrkanchana <66084534+jdrkanchana@users.noreply.github.com>
2020-12-04 14:56:15 +0530

    Create append_file.py

---
+++ file/append_file.py
@@ -0,0 +1,6 @@
+emp=open("employee.txt","a")
+#a appends to the file ,not overwrite
+#print file
+#here to end of the file this record Emily First Year is appended
+emp.write("Emily        First Year")
+emp.close()
